0bac1cd20dae473c30be8c67feadf0fee22b4550
Vivek <vivekmunde@users.noreply.github.com>
2026-01-19 14:36:32 +0530

    Pre-commit, lint & tsc checks

---
+++ eslint-config.js
@@ -0,0 +1,29 @@
+module.exports = [
+  {
+    ignores: ["coverage/**", "dist/**", "node_modules/**"]
+  },
+  {
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      parser: require("@typescript-eslint/parser"),
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        ecmaFeatures: { jsx: true }
+      }
+    },
+    plugins: {
+      "@typescript-eslint": require("@typescript-eslint/eslint-plugin"),
+      react: require("eslint-plugin-react"),
+      "react-hooks": require("eslint-plugin-react-hooks")
+    },
+    settings: {
+      react: {
+        version: "detect"
+      }
+    },
+    rules: {
+      "react/react-in-jsx-scope": "off"
+    }
+  }
+];
