798e7c7509920bb4837d03d63554601b419946a7
Vivek <vivekmunde@users.noreply.github.com>
2026-01-21 16:51:03 +0530

    Build config: clean & externals

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/index.tsx"],
+  entry: ["src/index.ts"],
   format: ["esm", "cjs"],
   dts: true,
   sourcemap: true,
-  clean: true
+  clean: true,
+  external: ["react", "react-dom"]
 });
